92d619ecd88fcb47c32c7df07eb8c228d5293a99
digilateraldev <digilateraldev@gmail.com>
2025-10-31 18:25:00 +0530

    Increase number of processes for animation generation

--- runner_multi.py
+++ runner_multi.py
@@ -4,4 +4,4 @@ img=Image.open('./examples/wan_animate/animate/shivam.png')
 width, height = img.size
 print(width,height)
 os.system(f'python3 ./wan/modules/animate/preprocess/preprocess_data.py --ckpt_path ./Wan2.2-Animate-14B/process_checkpoint --video_path ./examples/wan_animate/animate/5secvideo.mp4 --refer_path ./examples/wan_animate/animate/shivam.png --save_path ./wan/process_results --resolution_area {width} {height} --retarget_flag --use_flux')
-os.system(f'python -m torch.distributed.run --nnodes 1 --nproc_per_node 2 generate.py --task animate-14B --ckpt_dir ./Wan2.2-Animate-14B/ --src_root_path /workspace/Wan2.2/wan/process_results --refert_num 1 --dit_fsdp --t5_fsdp --ulysses_size 2')
+os.system(f'python -m torch.distributed.run --nnodes 1 --nproc_per_node 4 generate.py --task animate-14B --ckpt_dir ./Wan2.2-Animate-14B/ --src_root_path /workspace/Wan2.2/wan/process_results --refert_num 1 --dit_fsdp --t5_fsdp --ulysses_size 4')
