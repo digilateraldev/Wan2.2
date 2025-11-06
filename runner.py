e00c96676433211644c5951e12289cde28a899f5
digilateraldev <digilateraldev@gmail.com>
2025-11-06 16:25:32 +0530

    update runner.py

--- runner.py
+++ runner.py
@@ -1,7 +1,24 @@
 import os
 from PIL import Image
-img=Image.open('./examples/wan_animate/animate/arif-pose.png')
+import argparse
+from datetime import datetime
+
+# Get the current datetime object
+now = datetime.now()
+
+# Format the datetime object into a string suitable for a filename
+# Common format: YearMonthDay-HourMinuteSecond
+timestamp_str = now.strftime("%Y%m%d-%H%M%S")
+
+# Construct the filename
+processing_dir = f"./wan/process_results_{timestamp_str}"
+parser = argparse.ArgumentParser(description="Generate animation from a video and image using the Wan 2.2 animate")
+parser.add_argument('input_image_path',type=str, help="Path to the input photo file." )
+parser.add_argument('input_video_path'type=str, help="Path to the input video file.")
+args=parser.parse_args()
+# args.input_image_path
+img=Image.open(args.input_image_path)
 width, height = img.size
 print(width,height)
-os.system(f'python3 ./wan/modules/animate/preprocess/preprocess_data.py --ckpt_path ./Wan2.2-Animate-14B/process_checkpoint --video_path ./examples/wan_animate/animate/5secvideo.mp4 --refer_path ./examples/wan_animate/animate/arif-pose.png --save_path ./wan/process_results --resolution_area {width} {height} --retarget_flag --use_flux')
-os.system(f'python3 generate.py --task animate-14B --ckpt_dir ./Wan2.2-Animate-14B/ --src_root_path ./wan/process_results/ --refert_num 1')
+os.system(f'python3 ./wan/modules/animate/preprocess/preprocess_data.py --ckpt_path ./Wan2.2-Animate-14B/process_checkpoint --video_path ./examples/wan_animate/animate/{args.input_video_path} --refer_path ./examples/wan_animate/animate/{args.input_image_path} --save_path {processing_dir} --resolution_area {width} {height} --retarget_flag --use_flux')
+os.system(f'python3 generate.py --task animate-14B --ckpt_dir ./Wan2.2-Animate-14B/ --src_root_path {processing_dir} --refert_num 1')
