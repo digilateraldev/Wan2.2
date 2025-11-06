60ab5d7c14acb502b6633b0f4818e0cc83effd2e
digilateraldev <digilateraldev@gmail.com>
2025-11-06 16:40:46 +0530

    Add offload_model flag to generate.py command

--- runner.py
+++ runner.py
@@ -21,4 +21,4 @@ img=Image.open(args.input_image_path)
 width, height = img.size
 print(width,height)
 os.system(f'python3 ./wan/modules/animate/preprocess/preprocess_data.py --ckpt_path ./Wan2.2-Animate-14B/process_checkpoint --video_path ./examples/wan_animate/animate/{args.input_video_path} --refer_path ./examples/wan_animate/animate/{args.input_image_path} --save_path {processing_dir} --resolution_area {width} {height} --retarget_flag --use_flux')
-os.system(f'python3 generate.py --task animate-14B --ckpt_dir ./Wan2.2-Animate-14B/ --src_root_path {processing_dir} --refert_num 1')
+os.system(f'python3 generate.py --task animate-14B --ckpt_dir ./Wan2.2-Animate-14B/ --src_root_path {processing_dir} --refert_num 1 --offload_model True')
